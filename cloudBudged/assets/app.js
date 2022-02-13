595080d537a479eaf6b6bc8ecba470e7fdbdb096
Nihad512 <73600990+Nihad512@users.noreply.github.com>
2022-02-13 15:38:35 +0100

    Add files via upload

---
+++ cloudBudged/assets/app.js
@@ -0,0 +1,11 @@
+function deleteSlider() {
+    var element = document.getElementById('button')
+    element.classList.remove('active')
+}
+
+
+
+function slider() {
+    var element=document.getElementById('button')
+    element.classList.add("active")
+}
